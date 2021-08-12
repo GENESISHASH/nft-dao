17c54c0cdd5e16b729fc275867bfba569b7ee1dc
taky <taky@taky.com>
2021-08-12 12:34:46 -0400

    taky@auto

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -21,6 +21,9 @@ def main():
   publish_source = False
   account = accounts.load('devel')
 
+  # deploy dao
+  _dao = _dao.deploy('DAO',{'from':account},publish_source=publish_source)
+
   # deploy cryptopunks contract and claim a few punks
   _cryptopunks = cryptopunks.deploy({'from':account},publish_source=publish_source)
 
@@ -34,15 +37,12 @@ def main():
   _stable_token = stable_token.deploy("Stablecoin","PUSD",0,{'from':account},publish_source=publish_source)
   _vault = vault_cryptopunks.deploy("Vault",_stable_token,_cryptopunks,{"from":account},publish_source=publish_source)
 
-  # add the vault as a minter for the stablecoin
+  # add the vault and dao as a minter for the stablecoin
   _stable_token.add_minter(_vault,{'from':account})
+  _stable_token.add_minter(_dao,{'from':account})
 
-  # set punk prices
-  """
-  _vault.set_punk_value('floor',100000,{'from':account})
-  _vault.set_punk_value('ape',5000000,{'from':account})
-  _vault.set_punk_value('alien',10000000,{'from':account})
-  """
+  # mint 2m for the dao
+  _stable_token.mint(dao,2000000,{'from':account})
 
   # open a new position with my ape
   _vault.open_position(PUNK_INDEX_APE,{'from':account})
@@ -50,13 +50,13 @@ def main():
   # ..ui waits for deposit before borrow() is able to be called
   # ui can actually call _vault.get_punk_owner() until it's the vault address
 
-  # deposit the punk into the vault
+  # deposit the punk into the vault (user does this via web3)
   _cryptopunks.transferPunk(_vault,PUNK_INDEX_APE,{'from':account})
 
-  # borrow some stablecoin against it, this fails if we don't own the punk
+  # borrow some stablecoin against it now that we have it in vault
   _vault.borrow(PUNK_INDEX_APE,9000,{'from':account})
 
-  # make a payment against my ape
+  # make a payment against my ape loan
   _vault.repay(PUNK_INDEX_APE,500,{'from':account})
 
   #####################################
@@ -68,12 +68,13 @@ def main():
   if os.environ.get('NETWORK') ==  'ropsten':
     prefix = 'https://ropsten.etherscan.io/address/'
 
-  print("\n")
-  print("Finished deployment")
+  print("\n\n")
   print("wallet", prefix + str(account))
-  print("_cryptopunks", prefix + str(_cryptopunks))
-  print("_stable_token", prefix + str(_stable_token))
+  print('---')
+  print("_dao", prefix + str(_dao))
   print("_vault", prefix + str(_vault))
+  print("_stable_token", prefix + str(_stable_token))
+  print("_cryptopunks", prefix + str(_cryptopunks))
 
   return True
 
