89a77a0fa3c80f07daafa5ca2baff14a22a67843
taky <taky@taky.com>
2021-08-27 22:30:41 -0400

    taky@auto

--- deploy-ropsten.sh
+++ deploy-ropsten.sh
@@ -2,5 +2,5 @@
 export NETWORK=ropsten
 export WEB3_INFURA_PROJECT_ID=b7b474054fd840098a64dec665138997
 export ETHERSCAN_TOKEN=1IXN9XW64WASRZMGF1QCKMS1C6W5FTKJTY
-brownie run deploy --network ropsten
+brownie run test --network ropsten
 
